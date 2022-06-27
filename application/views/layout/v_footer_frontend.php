d69912685f01cfc9cc2dea9925ed080365b5a614
novrizal24 <mhmmdnovrizal@gmail.com>
2022-06-27 17:01:55 +0700

    first commit

---
+++ application/views/layout/v_footer_frontend.php
@@ -0,0 +1,32 @@
+</div>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+
+<!-- /.control-sidebar -->
+
+<!-- Main Footer -->
+<footer class="main-footer">
+	<!-- To the right -->
+	<div class="float-right d-none d-sm-inline">
+		Anything you want
+	</div>
+	<!-- Default to the left -->
+	<strong>Copyright &copy; 2019-2022 <a href="https://adminlte.io">TeamApotek.io</a>.</strong> All rights reserved.
+</footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<script>
+	window.setTimeout(function() {
+		$(".alert").fadeTo(500, 0).slideUp(500, function() {
+			$(this).remove();
+		});
+	}, 3000)
+</script>
+</body>
+
+</html>
